bfb2236393099b73c4680f4e1307577c9fc1d201
Imesh Nirmal <imeshnirmal1u@gmail.com>
2025-10-17 16:57:50 +0530

    style: Clean up imports and formatting in App component for improved readability

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
-import LandingPageBody from "./components/landing page/LandingPageBody"
-import NavBar from "./components/navigationBar/NavBar"
+import React, { useState } from "react";
+import ChatWidget from "./components/chat/ChatWidget";
+import LandingPageBody from "./components/landing page/LandingPageBody";
+import NavBar from "./components/navigationBar/NavBar";
 
 function App() {
-  
+  const [isChatOpen, setIsChatOpen] = useState(false);
 
   return (
     <>
-     <NavBar />
-     <LandingPageBody />
+      <NavBar />
+      <LandingPageBody />
+      <button onClick={() => setIsChatOpen(!isChatOpen)}>Chat</button>
+      {isChatOpen && <ChatWidget />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
